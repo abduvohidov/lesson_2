42289b910e232a6c1963220ceb41cd01d7c2be7f
Abdulloh <abduvohidov.abdullokh@gmail.com>
2024-08-03 20:20:32 +0500

    mongoose configuration had done

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const mongo = async () => {
-    return await mongoose.connect("mongodb://localhost://")
+    return await mongoose.connect("mongodb://localhost://27017/movies")
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import express from "express"
 import { router } from "./routes/index.js"
+import { mongo } from "mongoose"
 
 const app = express()
 const PORT = 9000
+mongo()
+    .then(() => console.log("db was  connected"))
+    .catch((err) => console.log(err))
 
 app.use(express.json())
 app.use("/", router)
